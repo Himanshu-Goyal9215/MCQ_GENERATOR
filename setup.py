6ae8596b9f00e7c732955f0bddb9a4abcfa1d30a
Himanshu <goyalhimanshu096@gmail.com>
2024-12-08 16:31:22 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+# for creating local packages
+# Why Use setup.py?
+# Makes your project installable.
+# Automatically handles dependencies.
+# Prepares your project for distribution.
+# Allows easy upload to PyPI.
+# Enables users to install your project via pip
---
+++ src/_init_.py
@@ -0,0 +1,7 @@
+# In Python, a directory with an __init__.py file is treated as a package.
+# The __init__.py file:
+
+# Makes a directory a Python package.
+# Allows you to include initialization code for your package.
+# Provides control over module exports.
+# Ensures backward compatibility with older Python versions.
